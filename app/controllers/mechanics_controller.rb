cbd055a76aed0c3303286cd8e41a4ae197984614
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-22 12:39:04 -0600

    Add functionality to add a ride thru a form on mechanic show page.

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,4 +6,11 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+
+  def new
+    mechanic = Mechanic.find(params[:id])
+    ride = MechanicRide.new(mechanic_id: params[:id], ride_id: params[:ride_id])
+    ride.save
+    redirect_to "/mechanics/#{mechanic.id}"
+  end
 end
