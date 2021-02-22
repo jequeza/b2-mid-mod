d91c0528c5c8a7facb91e31259ed4f4ac497bd40
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-22 11:54:58 -0600

    Add test to display mechanic info on show page.

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Mechanic Show Page" do
+  before :each do
+    Mechanic.destroy_all
+    Ride.destroy_all
+    MechanicRide.destroy_all
+
+   @salomon = Mechanic.create!(name: "Salomon", years_experience: 18)
+   @round_up = Ride.create!(name: "Round Up", thrill_rating: 8, open: true)
+   @scrambler = Ride.create!(name: "Scrambler", thrill_rating: 9)
+   @ferris_wheel =  Ride.create!(name: "Ferris Wheel", thrill_rating: 5, open: true)
+   @bumper_cars = Ride.create!(name: "Bumper Cars", thrill_rating: 8, open: true)
+   MechanicRide.create!(mechanic_id: @salomon.id, ride_id: @round_up.id)
+   MechanicRide.create!(mechanic_id: @salomon.id, ride_id: @scrambler.id)
+   MechanicRide.create!(mechanic_id: @salomon.id, ride_id: @ferris_wheel.id)
+   MechanicRide.create!(mechanic_id: @salomon.id, ride_id: @bumper_cars.id)
+end
+  describe "When I visit a mechanic show page" do
+    it "I see their name, years of experience, and the names of all rides they're working on" do
+      visit "/mechanics/#{@salomon.id}"
+
+      within "#mechanic-info" do
+        expect(page).to have_content(@salomon.name)
+        expect(page).to have_content(@salomon.years_experience)
+        expect(page).to have_content(@round_up.name)
+        expect(page).to have_content(@ferris_wheel.name)
+        expect(page).to have_content(@bumper_cars.name)
+      end
+    end
+    it "I only see rides taht are open" do
+      visit "/mechanics/#{@salomon.id}"
+
+      within "#mechanic-info" do
+        expect(page).to_not have_content(@scrambler.name)
+      end
+    end
+  end
+end
